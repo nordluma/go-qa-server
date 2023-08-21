4667f4a8f9264f0520cd969c1d3f6420f43a36a6
nordluma <maxnordlund.95@hotmail.com>
2023-08-21 20:23:24 +0300

    feat(questions): add router group for questions

--- main.go
+++ main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
+	"net/http"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
+func addQuestionRoutes(rg *gin.RouterGroup) {
+	questions := rg.Group("/questions")
+
+	questions.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, "questions")
+	})
+}
+
 func setupRoutes() *gin.Engine {
 	r := gin.Default()
-	r.GET("/health_check", func(ctx *gin.Context) {
+	r.Use(cors.Default())
+	routes := r.Group("/v1")
+	routes.GET("/health_check", func(ctx *gin.Context) {
 		ctx.Status(200)
 	})
+
+	addQuestionRoutes(routes)
 
 	return r
 }
