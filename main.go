3bb41376a8fcaee69427570054f38c4eb5a095bd
nordluma <maxnordlund.95@hotmail.com>
2023-11-16 22:32:06 +0200

    feat: connect to database

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/jmoiron/sqlx"
 	"github.com/nordluma/go-qa-server/routes"
+
+	_ "github.com/lib/pq"
 )
 
 func setupRoutes() *gin.Engine {
 	r := gin.Default()
 	r.Use(cors.Default())
+
 	r.GET("/health_check", healthCheck)
 
 	routesV1 := r.Group("/v1")
@@ -24,6 +30,14 @@ func healthCheck(ctx *gin.Context) {
 
 func main() {
 	r := setupRoutes()
+
+	_, err := sqlx.Connect(
+		"postgres",
+		"postgres://postgres:password@localhost/qadb",
+	)
+	if err != nil {
+		log.Fatal("Could not establis connection to database: ", err)
+	}
 
 	r.Run(":8080")
 }
